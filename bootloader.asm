52c09c2dc501e886834074e10a82e77f571ed084
Asko7779 <stefanecadrian@gmail.com>
2024-12-07 17:18:10 +0100

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -3,55 +3,50 @@
 
 ; Initial Setup in Real Mode
 START:
-    cli                 ; Disable interrupts
+    cli                
     mov ax, 0x0
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7C00      ; Setup stack
+    mov sp, 0x7C00      
 
-    ; Load Kernel (using BIOS interrupt)
-    mov ah, 0x02        ; Function to read sectors
-    mov al, 1           ; Read 1 sector
-    mov ch, 0           ; Cylinder 0
-    mov cl, 2           ; Start reading from sector 2
-    mov dh, 0           ; Head 0
-    mov dl, 0x80        ; First hard disk
-    int 0x13            ; BIOS Disk Interrupt
+    mov ah, 0x02        
+    mov al, 1           
+    mov ch, 0           
+    mov cl, 2          
 
-    ; Check for error in loading
-    jc load_error       ; Jump if error
-
-    ; Setup GDT and switch to protected mode
+    mov dh, 0           
+    mov dl, 0x80       
+    int 0x13            
+
+    jc load_error      
+
     lgdt [gdt_descriptor]
     mov eax, cr0
-    or eax, 0x1         ; Enable protected mode
+    or eax, 0x1          
     mov cr0, eax
     jmp 0x08:protected_mode
 
 load_error:
-    hlt                 ; Halt on error
-
+    hlt                 
 [BITS 32]
 protected_mode:
-    mov ax, 0x10        ; Data segment selector
+    mov ax, 0x10         
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, 0x90000    ; Setup stack in protected mode
-
-    ; Jump to kernel entry point
+    mov esp, 0x90000   
+
     jmp 0x10000
 
-; Define the Global Descriptor Table (GDT)
 gdt_start:
-    dq 0x0                      ; Null descriptor
-    dq 0x00CF9A000000FFFF       ; Code segment descriptor
-    dq 0x00CF92000000FFFF       ; Data segment descriptor
+    dq 0x0                      
+    dq 0x00CF9A000000FFFF       
+    dq 0x00CF92000000FFFF       
 
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1  ; GDT size
-    dd gdt_start                ; GDT address
+    dw gdt_end - gdt_start - 1  
+    dd gdt_start                
 
 gdt_end:
 
